d964bbab74293d1e501bfc8f6cd8a66bdb2c422b
webdesignbysubhasis6 <subhasis6paul2003@gmail.com>
2024-07-28 23:02:23 +0530

    rollback

--- src/api/index.js
+++ src/api/index.js
@@ -3,20 +3,20 @@ const API = axios.create({
   baseURL:process.env.REACT_APP_BACKEND_URL,
 });
 
-export const UserSignUp = async (data) => API.post("/signup", data);
-export const UserSignIn = async (data) => API.post("/signin", data);
+export const UserSignUp = async (data) => API.post("/user/signup", data);
+export const UserSignIn = async (data) => API.post("/user/signin", data);
 
 export const getDashboardDetails = async (token) =>
-  API.get("/dashboard", {
+  API.get("/user/dashboard", {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const getWorkouts = async (token, date) =>
-  await API.get(`/workout${date}`, {
+  await API.get(`/user/workout${date}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const addWorkout = async (token, data) =>
-  await API.post(`/workout`, data, {
+  await API.post(`/user/workout`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
